2436105d07da54e2e4f6e623eb19c663a3017cc1
Vladimir Kuznichenkov <vladimir.kuznichenkov@insly.com>
2018-05-12 17:41:14 +0300

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# vps
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,20 @@
+name 'vps'
+maintainer 'The Authors'
+maintainer_email 'kuzaxak.tech@gmail.com'
+license 'All Rights Reserved'
+description 'Installs/Configures vps'
+long_description 'Installs/Configures vps'
+version '0.1.0'
+chef_version '>= 12.14' if respond_to?(:chef_version)
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/vps/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/vps'
