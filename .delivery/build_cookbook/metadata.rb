e1e346f51f2ee0ef8bb0c41ef87ccb27f2b37213
Vladimir Kuznichenkov <vladimir.kuznichenkov@insly.com>
2018-05-12 17:41:15 +0300

    Add generated delivery build cookbook

---
+++ .delivery/build_cookbook/metadata.rb
@@ -0,0 +1,8 @@
+name 'build_cookbook'
+maintainer 'The Authors'
+maintainer_email 'kuzaxak.tech@gmail.com'
+license 'all_rights'
+version '0.1.0'
+chef_version '>= 12.14' if respond_to?(:chef_version)
+
+depends 'delivery-truck'
