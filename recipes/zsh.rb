daff73516de7347460536a388d54c3e05a63dfa9
Vladimir Kuznichenkov <vladimir.kuznichenkov@insly.com>
2018-05-14 11:34:29 +0300

    Add root console

--- recipes/zsh.rb
+++ recipes/zsh.rb
@@ -26,3 +26,32 @@ node.default['users'].each do |user|
       end
     end
 end
+
+## root
+
+template "/root/.zshrc" do
+  source 'zshrc.erb'
+  owner user
+  group user
+  mode '0744'
+  action :create
+end
+
+git "/root/.oh-my-zsh" do
+  repository 'https://github.com/robbyrussell/oh-my-zsh.git'
+  action :checkout
+  user user
+  group user
+  enable_submodules true
+  depth 1
+end
+
+node.default['oh-my-zsh']['plugins']['custom'].each do |name, repo|
+  git "/root/.oh-my-zsh/custom/plugins/#{name}" do
+    repository repo
+    action :checkout
+    user user
+    group user
+    depth 1
+  end
+end
